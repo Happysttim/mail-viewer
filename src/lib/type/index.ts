a6407504d8b3837e526d1beef48adde6791fc594
Happysttim <gmlsdyd1023@naver.com>
2025-03-07 19:07:21 +0900

    refactor: 타입 수정

--- src/lib/type/index.ts
+++ src/lib/type/index.ts
@@ -1,16 +1,16 @@
-import { ZodObject, ZodTypeAny } from "zod";
+import { TypeOf, z, ZodObject, ZodTypeAny } from "zod";
 
 export type Zod = ZodObject<{[key: string]: ZodTypeAny}>;
 export interface CommandMap {
     readonly __protocol: string;
 }
 
-export interface CommandResult<T extends CommandMap, C extends CommandName<T>, Z extends Zod> {
+export interface CommandResult<T extends CommandMap, C extends CommandName<T>, Z extends z.infer<Zod>> {
     command: C;
-    args: CommandArgs<T, C>, 
+    args: CommandArgs<T, C>,
     schema: Z
 }
 
-export type CommandName<T extends CommandMap> = keyof Omit<T, "__protocol">;
+export type CommandName<T extends CommandMap> = Extract<keyof Omit<T, "__protocol">, string>;
 export type CommandArgs<T extends CommandMap, K extends CommandName<T>> = T[K] extends (...args: any[]) => any
     ? Parameters<T[K]> : never;
