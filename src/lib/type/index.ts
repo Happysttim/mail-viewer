7b63911816d7c86a16f3f093171d219c0ea94953
Happysttim <gmlsdyd1023@naver.com>
2025-03-11 21:57:02 +0900

    refactor: Result 타입 추가 및 Tls, HostOption 타입 이동

--- src/lib/type/index.ts
+++ src/lib/type/index.ts
@@ -1,16 +1,32 @@
-import { TypeOf, z, ZodObject, ZodTypeAny } from "zod";
+import { z, ZodObject, ZodTypeAny } from "zod";
 
-export type Zod = ZodObject<{[key: string]: ZodTypeAny}>;
 export interface CommandMap {
     readonly __protocol: string;
 }
-
-export interface CommandResult<T extends CommandMap, C extends CommandName<T>, Z extends z.infer<Zod>> {
+export type Zod = ZodObject<{[key: string]: ZodTypeAny}>;
+export type CommandResult<T extends CommandMap, C extends CommandName<T>, Z extends z.infer<Zod>> = {
     command: C;
-    args: CommandArgs<T, C>,
-    schema: Z
+    args: CommandArgs<T, C>;
+    schema: Z;
 }
 
 export type CommandName<T extends CommandMap> = Extract<keyof Omit<T, "__protocol">, string>;
+export type Result<Map extends CommandMap, Command extends CommandName<Map>> = 
+    Map[Command] extends (...args: any) => infer S ? S : never;
+export type IdResult<Map extends CommandMap> = {
+    id: string,
+    commandResult: CommandResult<Map, CommandName<Map>, z.infer<Zod>>
+}
 export type CommandArgs<T extends CommandMap, K extends CommandName<T>> = T[K] extends (...args: any[]) => any
     ? Parameters<T[K]> : never;
+
+export type Tls = {
+    rejectUnauthorized?: boolean | undefined
+}
+
+export type HostOption = {
+    hostname: string;
+    port: number;
+    secure: boolean;
+    tls?: Tls;
+}
