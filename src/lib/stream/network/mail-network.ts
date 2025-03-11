3c1a292dacabef84f22bfc2b6dfe844fabb6fe05
Happysttim <gmlsdyd1023@naver.com>
2025-03-11 21:55:14 +0900

    refactor: EventEmitter 추가

--- src/lib/stream/network/mail-network.ts
+++ src/lib/stream/network/mail-network.ts
@@ -2,16 +2,11 @@ import { connect as tcp_connect, Socket } from "node:net";
 import { connect as tls_connect, TLSSocket } from "node:tls";
 import { pipeline } from "node:stream";
 import { CommandTransform } from "lib/stream/transform";
-import { HostOption } from "lib/object/network/host-option";
 import Receiver from "lib/stream/receiver";
-import { CommandMap } from "lib/type";
+import { CommandMap, HostOption } from "lib/type";
 import { Handler } from "./";
 import { Parser } from "lib/parser";
-import { streamEvent, StreamEvent } from "lib/event/stream";
-import { uid } from "uid";
-import log from "lib/logger";
-import { LogType } from "lib/logger/logger";
-import { SchemaEvent } from "lib/event";
+import EventEmitter from "node:events";
 
 export enum SocketStatus {
     CONNECTING = "CONNECTING",
@@ -19,53 +14,42 @@ export enum SocketStatus {
     DISCONNECT = "DISCONNECT",
     CONNECTED = "CONNECTED"
 }
-type SocketLike = Socket | TLSSocket | undefined;
 
-export class MailNetwork<T extends CommandMap> {
+type SocketLike = Socket | TLSSocket | undefined;
+type EventMap = {
+    "socket-status": (previous: SocketStatus, now: SocketStatus) => void;
+    "socket-error": (cause: string) => void;
+    "stream-pipeline-error": (cause: NodeJS.ErrnoException | null) => void;
+};
+type EventName = keyof EventMap;
+type EventCallbackArgs<K extends EventName> = Parameters<EventMap[K]>;
+
+export class MailNetwork<T extends CommandMap> extends EventEmitter {
 
     readonly hostOption: HostOption;
-    readonly protocol: string;
-    readonly prefix: string;
     readonly id: string;
 
-    private readonly tag = "MailNetwork";
-    
     private socket: SocketLike;
     private commandMap: T;
     private commandTransform: CommandTransform<T>;
     private parser: Parser<T>;
     private commandHandler: Handler<T> | undefined = undefined;
     private status: SocketStatus = SocketStatus.DISCONNECT;
-    private streamEvent: StreamEvent<T>;
-    private schemaEvent: SchemaEvent<T> | undefined;
 
     constructor(
-        protocol: string,
-        prefix: string,
-        hostOption: HostOption,
+        id: string,
         commandMap: T,
         commandTransform: CommandTransform<T>,
         parser: Parser<T>,
-        schemaEvent?: SchemaEvent<T>
+        hostOption: HostOption,
     ) {
+        super();
+        this.id = id;
+        this.commandMap = commandMap;
         this.commandTransform = commandTransform;
         this.parser = parser;
-        this.commandMap = commandMap;
-        this.protocol = protocol;
         this.hostOption = hostOption;
-        this.prefix = prefix;
-        this.id = this.prefix + uid(16);
-        this.schemaEvent = schemaEvent;
-        this.streamEvent = new StreamEvent();
         this.setSocketStatus(SocketStatus.CONNECTING);
-
-        this.initEvent();
-
-        streamEvent.emit("create-stream", this.protocol, this.hostOption, this.id);
-    }
-
-    event(schemaEvent: SchemaEvent<T>) {
-        this.schemaEvent = schemaEvent;
     }
 
     handler(): Handler<typeof this.commandMap> | undefined {
@@ -75,7 +59,7 @@ export class MailNetwork<T extends CommandMap> {
     connect(): Promise<void> {
         return new Promise((resolve, reject) => {
             try {
-                const receiver = new Receiver(this.id, this.commandTransform, this.parser, this.streamEvent);
+                const receiver = new Receiver(this.commandTransform, this.parser);
                 this.socket = this.hostOption.secure ? tls_connect(
                     {
                         host: this.hostOption.hostname,
@@ -91,14 +75,15 @@ export class MailNetwork<T extends CommandMap> {
         
                 this.socket.once("connect", () => {
                     this.setSocketStatus(SocketStatus.CONNECTED);
-                    this.commandHandler = new Handler(this.id, this.commandTransform, this.streamEvent);
+                    this.commandHandler = new Handler(this.commandTransform);
                     resolve();
                     pipeline(this.commandTransform, this.socket!!, receiver, err => {
-                        streamEvent.emit("stream-pipeline-error", this.id, err);
+                        this.emit("stream-pipeline-error", err);
                     });
                 });
         
                 this.socket.on("error", err => {
+                    this.emit("socket-error", err);
                     this.setSocketStatus(SocketStatus.ERROR);
                 });
             } catch(err) {
@@ -116,56 +101,26 @@ export class MailNetwork<T extends CommandMap> {
                 this.socket = undefined;
             });
         } else {
-            streamEvent.emit("socket-error", this.id, "Wrong disconnect request. cause: invalid socket");
+            this.emit("socket-error", "Wrong disconnect request. cause: invalid socket");
         }
+    }
+
+    on<K extends EventName>(eventName: K, listener: (...args: EventCallbackArgs<K>) => void): this {
+        return super.on(eventName, listener);
+    }
+
+    emit<K extends EventName>(eventName: K, ...args: EventCallbackArgs<K>): boolean {
+        return super.emit(eventName, ...args);
+    }
+
+    removeListener<K extends EventName>(eventName: K, listener: (...args: EventCallbackArgs<K>) => void): this {
+        return super.removeListener(eventName, listener);
     }
 
     private setSocketStatus(status: SocketStatus) {
         const previous = this.status;
         this.status = status;
-        streamEvent.emit("socket-status", this.id, previous, status);
-    }
-
-    private initEvent() {
-        this.streamEvent.on("command-handler-fail", id => {
-            log(
-                {
-                    tag: this.tag,
-                    type: LogType.INFO,
-                    context: `Stream(${id}) command handle is failed`,
-                }
-            );
-        });
-        this.streamEvent.on("command-handler-success", (id, commandName, args) => {
-            log(
-                {
-                    tag: this.tag,
-                    type: LogType.INFO,
-                    context: `Stream(${id}) command(${commandName.toString()} ${args}) handled`,
-                }
-            );
-        });
-        this.streamEvent.on("command-receiver-schema", (id, result) => {
-            if (this.schemaEvent) {
-                this.schemaEvent.emit(result.command, id, result);
-            }
-            log(
-                {
-                    tag: this.tag,
-                    type: LogType.INFO,
-                    context: `Stream(${id}) command(${result.command}) args(${result.args}) result: ${JSON.stringify(result.schema, null, 2)}`
-                }
-            );
-        });
-        this.streamEvent.on("command-receiver-schema-error", (id, result) => {
-            log(
-                {
-                    tag: this.tag,
-                    type: LogType.ERROR,
-                    context: `Stream(${id}) command(${result.command}) args(${result.args}) error has occurred`
-                }
-            );
-        });
+        this.emit("socket-status", previous, status);
     }
 
 }
