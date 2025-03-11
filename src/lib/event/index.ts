21ef1a09b65dbe830a136d05371409ead3a8aad6
Happysttim <gmlsdyd1023@naver.com>
2025-03-11 21:49:41 +0900

    feat: commandresult 이벤트 추가

--- src/lib/event/index.ts
+++ src/lib/event/index.ts
@@ -1,2 +1,21 @@
-export * from "./schema";
-export * from "./stream";
+import { EventEmitter } from "events";
+import { CommandMap, CommandName, Result } from "lib/type";
+import { uid } from "uid";
+
+class CommandEvent extends EventEmitter {
+
+    public generateCommandId(): string {
+        return uid(6);
+    }
+
+    once<Map extends CommandMap, Command extends CommandName<Map>>(eventName: string, listener: (error: boolean, result: Result<Map, Command>) => void): this {
+        return super.once(eventName, listener);
+    }
+
+    emit<Map extends CommandMap, Command extends CommandName<Map>>(eventName: string, error: boolean, result: Result<Map, Command>): boolean {
+        return super.emit(eventName, error, result);
+    }
+
+}
+
+export const commandEvent = new CommandEvent();
