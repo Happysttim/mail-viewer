5f3989dd93dce481e2c5dfee2ff0210d2d3f9387
Happysttim <gmlsdyd1023@naver.com>
2025-03-11 21:47:19 +0900

    fix: 잘못된 데이터베이스 경로 수정

--- src/lib/database/handle.ts
+++ src/lib/database/handle.ts
@@ -1,8 +1,8 @@
-import { User } from "./schema";
+import { UserDTO } from "./dto";
 import { withDatabase } from "./initialize";
 import fs from "node:fs";
 import { createHash } from "node:crypto";
-import { UserController } from "./controller/user";
+import { UserService } from "./service/user";
 
 export async function existsUser(id: string): Promise<boolean> {
     if (!fs.existsSync(`./data/users/${id}.db`)) {
@@ -10,21 +10,20 @@ export async function existsUser(id: string): Promise<boolean> {
     }
 
     return await withDatabase("./data/user.db", async database => {
-        return database.prepare<unknown[], User>("SELECT id FROM id = ?").get(id);
+        return database.prepare<unknown[], UserDTO>("SELECT id FROM UserTable WHERE id=?").get(id);
     }) !== undefined;
 }
 
-export async function user(user: User): Promise<UserController | undefined> {
+export async function user(user: UserDTO): Promise<UserService | undefined> {
     if (!existsUser(user.id)) {
         return undefined;
     }
-    const result = await withDatabase(`./data/users/${user.id}.db`, async database => {
-        database.pragma(`key='${user.password}'`);
-        return database.prepare<unknown[], User>("SELECT id FROM id = ? AND password = ?").get(
+    const result = await withDatabase("./data/user.db", async database => {
+        return database.prepare<unknown[], UserDTO>("SELECT id FROM UserTable WHERE id=? AND password=?").get(
             user.id,
             createHash("sha256").update(user.password).digest().toString("hex")
         );
     });
 
-    return result ? new UserController(result, `./data/users/${user.id}.db`) : undefined;
+    return result ? new UserService(user, `./data/users/${user.id}.db`) : undefined;
 }
