bda967bb9240bb8d6a3c6c9c0497c41a1ab87dba
Happysttim <gmlsdyd1023@naver.com>
2025-03-11 21:47:59 +0900

    refactor: dto 타입을 index로 옮김

---
+++ src/lib/database/dto/index.ts
@@ -0,0 +1,23 @@
+export type UserDTO = {
+    id: string;
+    password: string;
+}
+
+export type StreamDTO = {
+    streamId: string;
+    mailId: string;
+    mailPassword: string;
+    protocol: string;
+    host: string;
+    port: number;
+}
+
+export type MailDTO = {
+    mailId: number,
+    streamId: string;
+    uid: string;
+    isSeen: boolean;
+    date: string;
+    fromAddress: string;
+    subject: string;
+}
