2002945fa78b8f701fede250e9bd753e9440511f
Happysttim <gmlsdyd1023@naver.com>
2025-03-12 19:04:50 +0900

    build(eslint) : 플러그인 및 규칙 추가

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,37 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import stylisticJs from "@stylistic/eslint-plugin-js";
+import stylisticTs from "@stylistic/eslint-plugin-ts";
+import stylisticJsx from "@stylistic/eslint-plugin-jsx";
 
 export default tseslint.config(
     eslint.configs.recommended,
     tseslint.configs.recommended,
     {
+		ignores: [
+			"*.config.*js",
+			"*.config.*.ts",
+			"/node_modules/"
+		],
+		plugins: {
+			"@stylistic/ts": stylisticTs,
+			"@stylistic/jsx": stylisticJsx,
+			"@stylistic/js": stylisticJs,
+		},
       	rules: {
 			"@typescript-eslint/no-extra-non-null-assertion": ["off"],
 			"no-useless-escape": ["off"],
-			"@typescript-eslint/no-unused-vars": ["warn"],
+			"@typescript-eslint/no-unused-vars": ["off"],
+			"@stylistic/js/arrow-parens": ["error", "always"],
+			"@stylistic/js/no-extra-semi": ["error"],
+			"@stylistic/ts/semi": ["error", "always"],
+			"@stylistic/ts/quotes": [
+				"error", 
+				"double",
+				{
+					"allowTemplateLiterals": "avoidEscape"
+				} 
+			],
       	}
     }
 );
