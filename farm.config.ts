f400858d27dc4067476dea3dd38d522cc9c7e040
Happysttim <gmlsdyd1023@naver.com>
2025-03-15 21:05:13 +0900

    fix(farm) : external, externalnodebuiltins 설정

--- farm.config.ts
+++ farm.config.ts
@@ -1,26 +1,63 @@
 import { defineConfig } from "@farmfe/core";
 import electron from "@farmfe/js-plugin-electron"; 
+import path from "node:path";
 
 export default defineConfig({
+    server: {
+        port: 9000,
+        hmr: true,
+        cors: true,
+    },
     plugins: [
-        "@farmfe/plugin-react",
+        [
+            "@farmfe/plugin-react",
+            {
+                refresh: process.env.NODE_ENV === "development",
+                development: process.env.NODE_ENV === "development"
+            },
+        ],
         electron({
             main: {
-                input: "./app/index.ts",
+                input: path.join(__dirname, "./index.ts"),
+                farm: {
+                    compilation: {
+                        sourcemap: false,
+                        external: ["electron"],
+                        externalNodeBuiltins: true,
+                        output: {
+                            targetEnv: "node-next",
+                            path: "build",
+                            publicPath: "./",
+                        },
+                    }
+                }
             },
             preload: {
-                input: "./app/preload/index.ts",
+                input: path.join(__dirname, "./app/preload.ts"),
+                farm: {
+                    compilation: {
+                        sourcemap: false,
+                        external: ["electron"],
+                        externalNodeBuiltins: true,
+                        output: {
+                            targetEnv: "node-next",
+                            path: "build",
+                            publicPath: "./",
+                        }
+                    }
+                }
             },
         }),
     ],
     compilation: {
+        sourcemap: false,
         input: {
-            index: "./app/render/index.html",
+            login: path.join(__dirname, "./app/render/login.html"),
         },
         output: {
             path: "build",
-            publicPath: "/",
             targetEnv: "browser-esnext",
+            publicPath: "./",
         },
     },
 });
