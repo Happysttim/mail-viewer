2d6dbaed64cf977008b55f429629ef88ff6d3305
Happysttim <gmlsdyd1023@naver.com>
2025-03-25 01:15:44 +0900

    feat(preload) : request-win-control 추가

--- app/preload.ts
+++ app/preload.ts
@@ -1,7 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-// import { getObserve } from "app/config";
 import { MailFilterMap } from "app/type";
-import { contextBridge, ipcRenderer } from "electron";
+import { contextBridge, ipcRenderer, IpcRendererEvent } from "electron";
 import { MailDTO, ProfileDTO, StreamDTO, UserDTO } from "lib/database/dto";
 import { getObserve } from "./config";
 
@@ -22,20 +21,28 @@ type RequestMap = {
     "request-profile": void;
     "request-mail-list": string;
     "request-mail": number;
+    "request-win-control": "CLOSE" | "MINIMUM" | "MAXIMUM" | "UNMAXIMUM";
+};
+
+type ReceiveMap = {
+    "request-expand": (_: IpcRendererEvent, onExpand: boolean) => void;
 };
 
 type Keys<K> = keyof K;
 type MapParameter<T, K extends Keys<T>> = T[K] extends (...args: any[]) => any ? Parameters<T[K]> : never;
 type ReturnType<T, K extends Keys<T>> = T[K] extends (...args: any[]) => infer S ? S : never;
+type EventListener<T, K extends Keys<T>> = T[K] extends (event: IpcRendererEvent, ...args: any[]) => any ? T[K] : (event: IpcRendererEvent, ...args: any[]) => void;
 
 contextBridge.exposeInMainWorld("ipcRenderer", {
     request: <K extends Keys<RequestMap>>(channel: K, ...args: [RequestMap[K]]): void => ipcRenderer.send(channel, args),
     invoke: <K extends Keys<InvokeMap>>(channel: K, ...args: MapParameter<InvokeMap, K>): Promise<ReturnType<InvokeMap, K>> => ipcRenderer.invoke(channel, args),
+    on: <K extends Keys<ReceiveMap>>(channel: K, listener: EventListener<ReceiveMap, K>): Electron.IpcRenderer => ipcRenderer.on(channel, listener),
 });
 
 export interface IpcRenderer {
     request: <K extends Keys<RequestMap>>(channel: K, ...args: [RequestMap[K]]) => void,
     invoke: <K extends Keys<InvokeMap>>(channel: K, ...args: MapParameter<InvokeMap, K>) => Promise<ReturnType<InvokeMap, K>>,
+    on: <K extends Keys<ReceiveMap>>(channel: K, listener: EventListener<ReceiveMap, K>) => Electron.IpcRenderer,
 }
 
 contextBridge.exposeInMainWorld("observe", getObserve());
