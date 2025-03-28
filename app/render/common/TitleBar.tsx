4eb690a448a674b4538f139854e5004b74e9a16a
Happysttim <gmlsdyd1023@naver.com>
2025-03-29 01:22:49 +0900

    fix(titlebar) : symbol props 추가

--- app/render/common/TitleBar.tsx
+++ app/render/common/TitleBar.tsx
@@ -1,23 +1,28 @@
 import React, { useEffect, useState } from "react";
 
-const SymbolDown = () => {
+type Symbol = "MINIMUM" | "MAXIMUM" | "CLOSE";
+type TitleBarProp = {
+    symbols: Symbol[],
+};
+
+const SymbolMinimum = () => {
     return (
         <svg xmlns="http://www.w3.org/2000/svg"
             width="30"
             height="30"
             viewBox="0 0 30 30">
-            <path d="M8,15 h15" stroke="gray" strokeWidth="0.5" />    
+            <path d="M8,15 h15" stroke="white" strokeWidth="0.5" />    
         </svg>
     );
 };
 
-const SymbolExpand = () => {
+const SymbolMaximum = () => {
     return (
         <svg xmlns="http://www.w3.org/2000/svg"
             width="30"
             height="30"
             viewBox="0 0 30 30">
-            <path d="M10,10 H20 V20 H10 V10z" fill="transparent" stroke="gray" strokeWidth="0.5" />
+            <path d="M10,10 H20 V20 H10 V10z" fill="transparent" stroke="white" strokeWidth="0.5" />
         </svg>
     );
 };
@@ -28,24 +33,24 @@ const SymbolClose = () => {
             width="30"
             height="30"
             viewBox="0 0 30 30">
-            <path d="M10,10 l10,10 M20,10 l-10,10" stroke="gray" strokeWidth="0.5" />
+            <path d="M10,10 l10,10 M20,10 l-10,10" stroke="white" strokeWidth="0.5" />
         </svg>
     );
 };
 
-const SymbolDownsize = () => {
+const SymbolUnmaximum = () => {
     return (
         <svg xmlns="http://www.w3.org/2000/svg"
             width="30"
             height="30"
             viewBox="0 0 30 30">
-            <path d="M10,10 h10 v10 h-10 v-10z" fill="transparent" stroke="gray" strokeWidth="0.5" />
-            <path d="M13,10 v-3 h10 v10 h-3 m0,0z" fill="transparent" stroke="gray" strokeWidth="0.5" />
+            <path d="M10,10 h10 v10 h-10 v-10z" fill="transparent" stroke="white" strokeWidth="0.5" />
+            <path d="M13,10 v-3 h10 v10 h-3 m0,0z" fill="transparent" stroke="white" strokeWidth="0.5" />
         </svg>
     );
 };
 
-export const TitleBar = () => {
+export const TitleBar = ({ symbols }: TitleBarProp) => {
 
     const [ expand, setExpand ] = useState(false);
     const handleExpand = (action: "CLOSE" | "MINIMUM" | "MAXIMUM" | "UNMAXIMUM") => {
@@ -62,20 +67,34 @@ export const TitleBar = () => {
     }, []);
 
     return (
-        <div className="appRegionDrag fixed h-30 w-full bg-transparent top-0">
+        <div className="appRegionDrag fixed h-30 w-full bg-inherit top-0">
             <div className="flex justify-end">
                 <div className="appRegionNoDrag flex">
-                    <div className="titlebar_symbol hover:bg-gray-100" onClick={() => handleExpand("MINIMUM")}>
-                        <SymbolDown />
-                    </div>
-                    <div className="titlebar_symbol hover:bg-gray-100" onClick={() => handleExpand(expand ? "UNMAXIMUM" : "MAXIMUM")}>
-                        {
-                            expand ? <SymbolDownsize /> : <SymbolExpand />
-                        }
-                    </div>
-                    <div className="titlebar_symbol hover:bg-red-400" onClick={() => handleExpand("CLOSE")}>
-                        <SymbolClose />
-                    </div>
+                    {
+                        symbols.length > 0 && symbols.map((v) => {
+                            if (v === "MINIMUM") {
+                                return (
+                                    <div className="titlebar-symbol hover:bg-black/10" onClick={() => handleExpand("MINIMUM")}>
+                                        <SymbolMinimum />
+                                    </div>
+                                );
+                            }
+                            if (v === "MAXIMUM") {
+                                return (
+                                    <div className="titlebar-symbol hover:bg-black/10" onClick={() => handleExpand(expand ? "UNMAXIMUM" : "MAXIMUM")}>
+                                        {
+                                            expand ? <SymbolUnmaximum /> : <SymbolMaximum />
+                                        }
+                                    </div>
+                                );
+                            }
+                            return (
+                                <div className="titlebar-symbol hover:bg-red-400/50" onClick={() => handleExpand("CLOSE")}>
+                                    <SymbolClose />
+                                </div>
+                            );
+                        })
+                    }
                 </div>
             </div>
         </div>
