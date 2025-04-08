8766123cbf400df10556349068895fd66d2e2009
Happysttim <gmlsdyd1023@naver.com>
2025-04-08 23:15:33 +0900

    refactor(globalDT) : observe 삭제

--- app/global.d.ts
+++ app/global.d.ts
@@ -1,9 +1,7 @@
 import { IpcRenderer } from "./preload";
-import { Observe } from "./type";
 
 declare global {
     interface Window {
         ipcRenderer: IpcRenderer
-        observe: Observe
     }
 }
